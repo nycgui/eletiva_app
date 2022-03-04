9774180350de57545d540045b0db51f92b3279db
nycgui <nyc.gui@gmail.com>
2022-03-03 21:46:32 -0300

    controller atualizacao de user

---
+++ src/controllers/UpdateUserController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express'
+import { UpdateUserService } from '../services/UpdateUserService'
+
+class UpdateUserController {
+  async handle(request: Request, response: Response) {
+    // const id = request.params.id
+    const { id, name, email, admin, password } = request.body
+    const updateUserService = new UpdateUserService()
+
+    const user = await updateUserService.execute({
+      id,
+      name,
+      email,
+      admin,
+      password,
+    })
+    return response.json(user)
+  }
+}
+
+export { UpdateUserController }
