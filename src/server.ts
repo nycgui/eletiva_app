ecff6c2e9672ef7214ae373a3e2d2f5e7928d58c
nycgui <nyc.gui@gmail.com>
2022-03-17 19:38:48 -0300

    adptando server para middleware

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response, NextFunction } from "express";
+import 'express-async-errors'
 import {router} from './routes'
 
 const app = express()
@@ -6,4 +7,20 @@ const app = express()
 app.use(express.json())
 
 app.use(router)
+
+app.use(
+    (err: Error, request: Request, response: Response, next: NextFunction) => {
+      if (err instanceof Error) {
+        return response.status(400).json({
+          error: err.message
+        })
+      }
+
+      return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error",
+      })
+    }
+)
+
 app.listen(3000)
