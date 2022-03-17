9a391905cd94f84dcbaaca34bdba04a932c85ef9
nycgui <nyc.gui@gmail.com>
2022-03-17 19:37:57 -0300

    adicionando novas rotas

--- src/routes.ts
+++ src/routes.ts
@@ -3,16 +3,25 @@ import { CreateUserController } from "./controllers/CreateUserController"
 import { ListUserController } from "./controllers/ListUserController" 
 import { DeleteUserController } from "./controllers/DeleteUserController"
 import { UpdateUserController } from "./controllers/UpdateUserController"
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController"
+
+import { ensureAdmin } from "./middlewares/ensureAdmin"
+import { ensureAuthenticated } from "./middlewares/ensureAuthenticated"
 
 const createUserController = new CreateUserController()
 const listUserController = new ListUserController()
 const deleteUserController = new DeleteUserController()
 const updateUserController = new UpdateUserController()
+const authenticateUserController = new AuthenticateUserController()
 
 const router = Router()
 
 router.post('/users', createUserController.handle)
-router.get('/users', listUserController.handle)
+router.post('/login', authenticateUserController.handle)
+
+router.get('/users', ensureAuthenticated, ensureAdmin, listUserController.handle)
+
+router.use(ensureAdmin)
 router.delete('/users/:id', deleteUserController.handle)
 router.put('/users', updateUserController.handle)
 
